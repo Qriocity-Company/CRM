a938eadac23db110863e06d2e0aba23b619a3984
thelostwarrior69 <91337266+AdityaCoder-N@users.noreply.github.com>
2023-12-16 12:06:32 +0530

    Dates added

--- crm-frontend/src/components/Customers/Customers.jsx
+++ crm-frontend/src/components/Customers/Customers.jsx
@@ -34,7 +34,7 @@ const Customers = () => {
 
         <h1 className='font-semibold text-2xl'>Customer Messages Received</h1>
 
-        <div className='grid grid-cols-5 bg-[#2f2a7a] text-white mt-4'>
+        <div className='grid grid-cols-6 bg-[#2f2a7a] text-white mt-4'>
             <div className='col-span-1 p-4 text-xl font-bold'> 
                 Name
             </div>
@@ -44,6 +44,9 @@ const Customers = () => {
             <div className='col-span-1 p-4 text-xl font-bold text-left'>
                 Phone Number
             </div>
+            <div className='col-span-1 p-4 text-xl font-bold text-left'>
+                Date
+            </div>
             <div className='col-span-1 p-4 text-xl font-bold'>
                 
             </div>
@@ -52,7 +55,7 @@ const Customers = () => {
         {
             customers.map((customer,index)=>{
                 return(
-                    <div className='grid grid-cols-5 bg-blue-200 border-2 border-b-gray-300'>
+                    <div className='grid grid-cols-6 bg-blue-200 border-2 border-b-gray-300' id={index}>
                         <div className='col-span-1 p-4 font-semibold'> 
                             {customer.name}
                         </div>
@@ -61,6 +64,9 @@ const Customers = () => {
                         </div>
                         <div className='col-span-1 p-4 pl-10 text-xl font-bold text-left'>
                             {customer.phoneNumber}
+                        </div>
+                        <div className='col-span-1 p-4 pl-10 text-xl font-bold text-left'>
+                            {customer.date?customer.date:'N/A'}
                         </div>
                         <div className='col-span-1 p-4'>
                             <div className='py-2 bg-red-500 text-white font-semibold text-center rounded-xl cursor-pointer hover:bg-red-700' onClick={()=>deleteCustomer(customer.id)}>
